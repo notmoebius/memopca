286aa1129d47b54589a722424d40c3bac62984d0
Julien Gallerand <julien.gallerand@assurance-maladie.fr>
2021-04-27 16:34:06 +0200

    Mémo fonctionnel sans validations

--- src/Controller/MemoController.php
+++ src/Controller/MemoController.php
@@ -80,6 +80,12 @@ class MemoController extends AbstractController
             $em->persist($memo);
             $em->flush();
             $_POST = array();
+
+            return $this->render('memo/detailsMemo.html.twig', [
+                'login'=> $login,
+                'status' => $status,
+                'memo' => $memo,
+                ]);
             
         }
         return $this->render('memo/addMemo.html.twig', [
@@ -109,4 +115,85 @@ class MemoController extends AbstractController
         ]);
 
     }
+
+    public function UpdateMemo(Request $request): Response
+    {
+        
+        $login = $this->getUser();
+        $status = $this->getUser()->getStatus();
+
+        $em = $this->getDoctrine()->getManager();
+        $user= $em->getRepository(User::class)->findAll();
+        $memo = $em->getRepository(Memo::class)->findOneBy(['id' => $_GET['memo']]);
+
+        if(!empty($_POST)){
+
+            $safe= array_map('trim', array_map('strip_tags', $_POST));
+            $user= $em->getRepository(User::class);
+
+            
+            // Je recupere mes données de role et de grade
+            $safe['users'] = $user->FindOneBy(['id' => $safe['users']]);
+            $safe['informed'] = $user->FindOneBy(['id' => $safe['informed']]);
+            $safe['informed2'] = $user->FindOneBy(['id' => $safe['informed2']]);
+            $safe['inform'] = $user->FindOneBy(['id' => $safe['inform']]);
+            $safe['inform2'] = $user->FindOneBy(['id' => $safe['inform2']]);
+
+            
+            
+            $memo->setUsers($safe['users']);
+            $memo->setInformed1($safe['informed']);
+            
+            $memo->setInformed2($safe['informed2']);
+            
+            $memo->setInform1($safe['inform']);
+            
+            $memo->setInform2($safe['inform2']);
+            
+            $memo->setUpdatedAt(new \DateTime('now'));
+
+            if($request->isMethod('POST'))
+
+            $login = $this->getUser();
+            $status = $this->getUser()->getStatus();
+            
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($memo);
+            $em->flush();
+            $_POST = array();
+            
+            return $this->render('memo/detailsMemo.html.twig', [
+                'login'=> $login,
+                'status' => $status,
+                'memo' => $memo,
+                
+                ]);
+        }
+        return $this->render('memo/updateMemo.html.twig', [
+            'user' =>$user,
+            'memo' => $memo,
+            'login' => $login,
+            'status' => $status,
+        ]);
+    }
+
+        // SUPPRESSION MEMO
+
+        public function DeleteMemo(): Response
+        {
+    
+            $login = $this->getUser();
+            $status = $this->getUser()->getStatus();
+    
+            $em = $this->getDoctrine()->getManager();
+            $memo= $em->getRepository(Memo::class)->findOneBy(['id'=> $_GET['memo']]);
+    
+            $em->remove($memo);
+    
+            $em->flush();
+    
+            $this->addFlash('success',  'Le mémo a bien été supprimé.');
+    
+            return $this->redirectToRoute('list_memo_controller');
+        }
 }
