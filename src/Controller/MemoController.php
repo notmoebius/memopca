b51d1335ea4ea8f959fc4a82ace10d5223561beb
Julien Gallerand <julien.gallerand@assurance-maladie.fr>
2021-04-29 16:33:10 +0200

    Export Mémo en JSON - toujours a la recherche du bon format

--- src/Controller/MemoController.php
+++ src/Controller/MemoController.php
@@ -218,44 +218,21 @@ class MemoController extends AbstractController
                 $serializer = new Serializer();
                 $data['qui'] = "id".$memo->getUsers()->getId();
 
-            if($memo->getInformed1()->getId() == null){
-                $data['prevenu'] = ["", "id".$memo->getInformed2()->getId()];
-                $data['previent'] = ["id".$memo->getInform1()->getId(), "id".$memo->getInform2()->getId()];
-            
-            }
-            if($memo->getInformed2()->getId() == null){
-
-                $data['prevenu'] = ["id".$memo->getInformed1()->getId(), ""];
-                $data['previent'] = ["id".$memo->getInform1()->getId(), "id".$memo->getInform2()->getId()];
-
-            }
-            if($memo->getInform1()->getId() == null){
-
-                $data['prevenu'] = ["id".$memo->getInformed1()->getId(), "id".$memo->getInformed2()->getId()];
-                $data['previent'] = ["", "id".$memo->getInform2()->getId()];
-
-            }
-            if($memo->getInform2()->getId() == null){
-
-                $data['prevenu'] = ["id".$memo->getInformed1()->getId(), "id".$memo->getInformed2()->getId()];
-                $data['previent'] = ["id".$memo->getInform1()->getId(), ""];
-            
-            }
-            if(is_null($memo->getInform2()->getId()) == false || is_null($memo->getInform1()->getId()) == false || is_null($memo->getInformed2()->getId()) == false || is_null($memo->getInformed1()->getId()) == false){
                 $data['prevenu'] = ["id".$memo->getInformed1()->getId().", "."id".$memo->getInformed2()->getId()];
-                $data['previent'] = (["id".$memo->getInform1()->getId().", "."id".$memo->getInform2()->getId().""]);
+                $data['previent'] = ["id".$memo->getInform1()->getId().", "."id".$memo->getInform2()->getId().""];
+                $response = new JsonResponse();
+            
+                $response->headers->set('Content-Type', 'application/json');
+                $response->setEncodingOptions(JSON_UNESCAPED_UNICODE);
+                $response->setData($data);
+                return $response;
+            
             }  
 // dd($data);
-            $response = new JsonResponse();
-            
-            $response->headers->set('Content-Type', 'application/json');
-            $response->setEncodingOptions(JSON_UNESCAPED_UNICODE);
-            $response->setData($data);
-            return $response;
-          
-            
+
+
         }
 
-        
+
     }
-}
+
