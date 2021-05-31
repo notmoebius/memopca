a43652c2cba68996415a3ea8e864f300df118ac3
Julien Gallerand <julien.gallerand@assurance-maladie.fr>
2021-05-31 15:55:41 +0200

    Creation salle de crise

--- config/bundles.php
+++ config/bundles.php
@@ -10,9 +10,6 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Cnam\AuthSrvSecBundle\AuthSrvSecBundle::class => ['all' => true],
-    Cnam\CryptographieBundle\CryptographieBundle::class => ['all' => true],
-    Cnam\AuthAppBundle\AuthAppBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     KevinPapst\AdminLTEBundle\AdminLTEBundle::class => ['all' => true],
 ];
